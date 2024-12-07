6b9cb42e27bd851470cb7b27025568940fde94a1
Angga Artupas <tupaskeren@gmail.com>
2024-12-08 02:22:02 +0700

    updating ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,7 @@
+const { uptime } = require("process");
+const tagall = require("./tagall");
+const antiSpamLink = require("../middleware/antiSpamLink");
+
 module.exports = async (client, message) => {
     const startTime = Date.now();
     await message.reply('*Pong!*\n');
